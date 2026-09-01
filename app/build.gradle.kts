825c164177727bb8611e9cc53aca2b80b9a1960b
ryanijitari <ryanilestari@gmail.com>
2026-09-01 09:24:28 +0700

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,26 @@
-plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
 android {
     namespace = "id.jitari.autopostalarm"
     compileSdk = 35
-    defaultConfig { applicationId = "id.jitari.autopostalarm"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "1.0.1" }
+
+    defaultConfig {
+        applicationId = "id.jitari.autopostalarm"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.0.2"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
